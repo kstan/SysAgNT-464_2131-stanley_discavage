62d014a5fc82bcd8693af11dc6797663be4b529b
Kevin Stanley <kevmstan@gmail.com>
2013-11-02 21:38:54 -0400

    Initial PowerShell...
    Initial PowerShell script to collect information about system and store
    it in a file in the same directory.

--- sysAgnt.ps1
+++ sysAgnt.ps1
@@ -1,1 +1,16 @@
-get-process
+$Logfile = "sysAgnt.log"
+#Function to write to logfile.
+Function writeLog
+{
+   Param ([string]$logstring)
+   Add-content $Logfile -value $logstring
+}
+
+$computerName = $env:COMPUTERNAME #Gets computer name from ENV variable
+$date = Get-Date #Gets date.
+
+writeLog("Name of computer: $computerName")
+writeLog("Current Time: $date")
+writeLog("List of processes:")
+Get-Process | Select-Object name | Foreach { writeLog("$_")} #loop through all the processes and write each one to log
+writeLog("End of logfile.")
