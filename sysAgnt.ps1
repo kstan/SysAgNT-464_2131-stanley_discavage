ae707be9b23810f73d0cc9824c3e4587e7fcb731
csd2513 <csd2513@rit.edu>
2013-12-16 16:21:54 -0500

    formatting
    changed formatting for output of the sysagnt code

--- sysAgnt.ps1
+++ sysAgnt.ps1
@@ -12,13 +12,13 @@ Function writeLog
 
 
 $usrName = $env:username
-writeLog ("User name is: $usrName")
+writeLog ("Username|$usrName")
 
 $OSname = (Get-WmiObject Win32_OperatingSystem).Name
 $Architecture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
 
-writeLog("OS Name is: $OSname")
-writeLog("OS Architecture is: $Architecture")
+writeLog("OS Name|$OSname")
+writeLog("OS Architecture|$Architecture")
 
 
 
@@ -26,14 +26,14 @@ $computerName = $env:COMPUTERNAME #Gets computer name from ENV variable
 $date = Get-Date #Gets date.
 #$apps = Get-WmiObject -Class win32_Product | Select-Object name  #get list of apps
 
-writeLog("Name of computer: $computerName")
-writeLog("Current Time: $date")
+writeLog("Computer Name|$computerName")
+writeLog("Current Time|$date")
 
 #Prints above information formatted with comma as delimiter
 #writes user name, OS name, 32 or 64 bit, computer name, date and time to file 
-writeLog("List of info: $usrName,$OSname,$Architecture,$computerName,$date")
+#writeLog("List of info: $usrName,$OSname,$Architecture,$computerName,$date")
 
-writeLog("List of processes:")
+
 [string]$newtemp = ""
 [int]$num = 0
 Get-Process | Select-Object name | Foreach { 
@@ -44,15 +44,15 @@ Get-Process | Select-Object name | Foreach {
 	[string]$proclist = $proclist + $process + ","
 	}
 	
-	writeLog ("Let's see if this works: $proclist")
-	writeLog("List of applications")
+	writeLog ("Processes|$proclist")
+	
 Get-WmiObject -Class win32_Product | Select-Object name | Foreach {   #adds each application appending it to a string with a comma as a delimiter and new line at end of string
 	[string]$apprun = "$_"
 	[string]$apprun2 = $apprun.trimstart("@{name=")
 	[string]$apprun3 = $apprun2.trimend("}")
 	[string]$applist = $applist + $apprun3 + ","
 	}
-	writeLog("Let's see if this works: $applist")
+	writeLog("Applications|$applist")
 	
 	
 writeLog("End of logfile.")
