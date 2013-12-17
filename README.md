5773d123477e29fc53a9b4ad25f7f9d6847e5244
Kevin Stanley <kevmstan@gmail.com>
2013-12-17 03:29:26 -0500

    Updated PHP code and Documentation
    Final before presentation... updated documentation so that is is
    accurate to current build.  Also updated PHP and CSS.

--- README.md
+++ README.md
@@ -9,41 +9,60 @@ Requirements: Powershell
 Description
 ============
 
-SysAgNT is a Windows Profiler designed as an agent that can be placed on a computer and can call back
-to a centralized server where the data collected can be organized. 
+SysAgNT is a Windows Profiler designed as an remote PowerShell script is is run on a series of systems.  This data is then viewable through a web interface.
 
-The information it collects includes:
+The information it collects includes (currently):
 
 applications/services on the system
-applications/services in use
+services in use
+who is logged into the system
+OS type
+OS architecture
+computer name
+
+To be added:
 times system is typically on
 times system is in use
 times system is idle
 times system is logged into
-user logged into system
 
 All this information is organized so that a profile of the system can be created to help system administrators
 and IT professionals to better know when to run patches, secure the system and recognize suspicious activity.
 
-
-
 Installation
 ============
 
+For Clients:
+In order to run remotely, the following should be done:
+(open up Powershell as Administrator) and type
+Enable-PSRemoting
+(say Yes to all questions).
+Also, ensure that the network type is set as "Work."  If it is set to anything else, it will not be able to run scripts remotely.
+If the network type is already set, run the following in a script in order to set the network type back to "Work":
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	$nlm = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
+	$connections = $nlm.getnetworkconnections()
+	$connections |foreach {
+		if ($_.getnetwork().getcategory() -eq 0)
+		{
+			$_.getnetwork().setcategory(1)
+		}
+	}
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+If there is still trouble, the command "Set-WSManQuickConfig" can be run in PowerShell to setup remote settings.
+	
+For Server:
+The server must be running PHP in order for the web interface to work.
+Inside the root web folder the Powershell script creates a folder called "data."
+This is where all the logs are stored.
+Logs are organized by folder (computer name) and then the files are set as the date of when the log is run.
+
+To Run Custom Powershell Commands:
 Run Powershell as Administrator (right click, run as administrator)
-
 Type following command in Powershell:
-
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-
 If not run as administrator, there will be an error.
 
-
-MORE WILL BE ADDED...
-
-
-Upgrade Notice
-==============
-
-
+To setup SysAgNT to run every hour:
+Search "Scheduled Tasks" and open.
 
--- utils.php
+++ utils.php
@@ -5,9 +5,9 @@ class Utils
 {
 	// Name: includeHeader()
 	// Purpose: includes HTML header
-	// Parameters: 
+	// Parameters: pageTitle, styleLink, onLoad
 	// Returns: string with HTML header
-	static function includeHeader($pageTitle="", $styleLink="", $onLoad="")
+	static function includeHeader($pageTitle="", $styleLink="styles/main.css", $onLoad="")
 	{
 		$website="SysAgNT";
 		$header="
@@ -37,29 +37,172 @@ class Utils
 		return $footer;
 	}
 
-	static function getInfo($computer="")
+	// Name: includeNav()
+	// Purpose: includes navigation bar and title
+	// Parameters: 
+	// Returns: string with navigation
+	static function includeNav()
 	{
-		$cmd = "Invoke-Command -ComputerName $computer {ps}";
-		$output = shell_exec("powershell -command $cmd");
-		echo('<pre>');
-		echo($output);
-		echo('</pre>');
-		$cmd = "Invoke-Command -ComputerName $computer {Get-WmiObject -Class win32_Product}";
-		$output = shell_exec("powershell -command $cmd");
-		echo('<pre>');
-		echo($output);
-		echo('</pre>');
-		$cmd = "Invoke-Command -ComputerName $computer {(Get-WmiObject Win32_OperatingSystem).Name}";
-		$output = shell_exec("powershell -command $cmd");
-		echo('<pre>');
-		echo($output);
-		echo('</pre>');
-		$cmd = "Invoke-Command -ComputerName $computer {(Get-WmiObject Win32_OperatingSystem).OSArchitecture}";
-		$output = shell_exec("powershell -command $cmd");
-		echo('<pre>');
-		echo($output);
-		echo('</pre>');
+		$nav = "
+			<div class='divTitle'>
+				<h1 id='title'>SysAgNT</h1>
+			</div>
+			<div id='nav'>
+				<ul class='menu'>
+					<li><a href='index.php?page=Home'>Home</a></li>
+					<li><a href='index.php?page=ComputerList'>Computer List</a></li>
+					<li><a href='index.php?page=About'>About</a></li>
+				</ul>
+		</div>
+		";
+		return $nav;
 	}
+
+	// Name: printAbout()
+	// Purpose: prints About text file
+	// Parameters: 
+	// Returns: nothing
+	static function printAbout()
+	{
+		$about = file("about.txt");
+
+		foreach ($about as $line_num => $line)
+		{
+			$aboutText = $line;
+		}
+
+		print "<p>$aboutText</p>";
+	}
+
+	// Name: getComputerList
+	// Purpose: prints and gets list of computers from data folder
+	// Parameters: 
+	// Returns: array of computers
+	static function getComputerList()
+	{
+		$path = 'data';
+		$computerList = array();
+
+		//itnerates through dirctory and finds directories
+		foreach (new DirectoryIterator($path) as $file)
+		{
+			if($file->isDot()) continue; // is a file
+			if($file->isDir()) // is a directory
+			{
+				$computerList[] = $file->getFilename(); //adds to array of computers
+			}
+		}
+
+		print "<ul id='computerList'>"; //starts unordered list
+		foreach ($computerList as $computer)
+		{
+			print "<li><a href='index.php?page=ComputerList&computer=$computer'>$computer</a></li>";
+		}
+		print "</ul>"; //ends unordered list
+
+		return $computerList;
+	}
+
+	// Name: getLogList()
+	// Purpose: prints list of logs inside computer folder
+	// Parameters: $computerName
+	// Returns: nothing
+	static function getLogList($computerName="")
+	{
+		$path = "data/$computerName";
+		$logList = array();
+		//itnerates through dirctory and fines directories
+		foreach (new DirectoryIterator($path) as $file)
+		{
+			if ($file->getFilename() != "." && $file->getFilename() != "..") //strips out .. and .
+			{
+				$logList[] = $file->getFilename(); //adds to array of computers
+			}
+		}
+		print "<ul id='logList'>";
+		foreach ($logList as $log)
+		{
+			print "<li><a href='index.php?page=ComputerList&computer=$computerName&date=$log'>$log</a></li>";
+		}
+		print "</ul>";
+
+	}
+
+	// Name: getData()
+	// Purpose: gathers, organizes, and prints data from logfile
+	// Parameters: $computerName and $logName
+	// Returns: nothing
+	static function getData($computerName="",$logName="")
+	{
+		$file = file("data/$computerName/$logName");
+
+		$computer = array
+		(
+			"Username" => "",
+			"OS Name" => "",
+			"OS Architecture" => "",
+			"Computer Name" => "",
+			"Log Time" => "",
+			"Processes" => "",
+			"Applications" => ""
+		);
+
+		//Separates Data into Nice Array
+
+		$username = explode('|', $file[0]); //seperates username
+		$username[1] = trim(preg_replace('/\s+/', ' ', $username[1])); //removes newlines
+		$computer["Username"] = $username[1]; //adds username to array
+
+		$osName = explode('|', $file[1]); //separates OS Name
+		$osName[1] = trim(preg_replace('/\s+/', ' ', $osName[1])); //removes newlines
+		$computer["OS Name"] = $osName[1]; //adds OS Name to Array
+
+		$osArc = explode('|', $file[2]); //separates OS Name
+		$osArc[1] = trim(preg_replace('/\s+/', ' ', $osArc[1])); //removes newlines
+		$computer["OS Architecture"] = $osArc[1]; //adds OS Arch to Array
+
+		$computerName = explode('|', $file[3]); //separates Computer Name
+		$computerName[1] = trim(preg_replace('/\s+/', ' ', $computerName[1])); //removes newlines
+		$computer["Computer Name"] = $computerName[1]; //adds Computer Name to Array
+
+		$currentTime = explode('|', $file[4]); //separates Log Time
+		$currentTime[1] = trim(preg_replace('/\s+/', ' ', $currentTime[1])); //removes newlines
+		$computer["Log Time"] = $currentTime[1]; //adds Log Time to Array
+
+		$processes = explode('|', $file[5]); //separates Computer Name
+		$processes = explode(',', $processes[1]);
+		$blank = array_pop($processes); //removes blank
+		$computer["Processes"] = $processes; //adds processes to Array
+
+
+		$applications = explode('|', $file[6]); //separates Computer Name
+		$applications = explode(',', $applications[1]);
+		$blank = array_pop($applications); //removes blank
+		$computer["Applications"] = $applications; //adds applications to Array
+
+		print "<div class='computerInfo'>";
+		print "<h3 class='computerTitle'>" . $computer["Username"] . " @ " . $computer["Computer Name"] . "</h3>";
+		print "<h4 class='computerType'>" . $computer["OS Name"] . " " . $computer['OS Architecture'] . "</h4>";
+		print "<h4 class='computerType'>" . "Log Time: " . $computer["Log Time"] . "</h4>";
+		print "<h5 class='processesTitle'>" . "Proccesses" . "</h5>";
+		print "<ul class='longList'>";
+		foreach ($computer["Processes"] as $process)
+		{
+			print "<li class='longList'>$process</li>";
+		}		
+		print "</ul>";
+		print "<h5 class='processesTitle'>" . "Applications" . "</h5>";
+		print "<ul class='longList'>";
+		foreach ($computer["Applications"] as $app)
+		{
+			print "<li class='longList'>$app</li>";
+		}		
+		print "</ul>";
+
+		print "</div>";
+	}
+
+
 }
 
 ?>
