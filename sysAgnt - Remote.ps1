1e89cdf74e711617001f959b62da785143b9d086
csd2513 <csd2513@rit.edu>
2013-12-17 03:32:53 -0500

    added remote functionality
    can obtain information from client remotely and can transfer log file to
    directory that links to web GUI

--- sysAgnt - Remote.ps1
+++ sysAgnt - Remote.ps1
@@ -1,7 +1,7 @@
 #Windows Testing Environment set up , Windows 7 and Server 2012 (acting as Domain Controller)
 
 
-$Logfile = "sysAgnt.log"
+$Logfile = "C:\Users\Administrator\Desktop\GitHub\SysAgNT-464_2131-stanley_discavage\sysAgntRemote.log"
 
 #Function to write to logfile.
 Function writeLog
@@ -11,23 +11,40 @@ Function writeLog
 }
 
 
-$usrName = $env:username
+$computerlist = Get-Content Hostnames.txt
+
+foreach ($computer in $computerlist)
+{
+
+#if (!([system.io.directory]::Exists(C:\Users\SysAgNT\site\Dropbox\site\data\$computer)))
+#{
+ #  [system.io.directory]::CreateDirectory((C:\Users\SysAgNT\site\Dropbox\site\data\$computer))
+ #}
+ 
+ $check = Test-Path -PathType Container C:\Users\SysAgNT\site\Dropbox\site\data\$computer
+if($check -eq $false){
+    New-Item "C:\Users\SysAgNT\site\Dropbox\site\data\$computer" -type Directory
+}
+
+$usrName = invoke-command -computername $computer {$env:username}
 writeLog ("Username|$usrName")
 
-$OSname = (Get-WmiObject Win32_OperatingSystem).Name
-$Architecture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+$OSname = invoke-command -computername $computer {(Get-WmiObject Win32_OperatingSystem).Name}
+$Architecture = invoke-command -computername $computer {(Get-WmiObject Win32_OperatingSystem).OSArchitecture}
 
 writeLog("OS Name|$OSname")
 writeLog("OS Architecture|$Architecture")
 
 
 
-$computerName = $env:COMPUTERNAME #Gets computer name from ENV variable
-$date = Get-Date #Gets date.
-#$apps = Get-WmiObject -Class win32_Product | Select-Object name  #get list of apps
+$computerName = invoke-command -computername $computer {$env:COMPUTERNAME} #Gets computer name from ENV variable
+$date = invoke-command -computername $computer {Get-Date -format u} #Gets date.
+$datename = invoke-command -computername $computer {(get-date).tostring("MMddyyyyHHmmss")}
+
 
 writeLog("Computer Name|$computerName")
 writeLog("Current Time|$date")
+
 
 #Prints above information formatted with comma as delimiter
 #writes user name, OS name, 32 or 64 bit, computer name, date and time to file 
@@ -36,24 +53,33 @@ writeLog("Current Time|$date")
 
 [string]$newtemp = ""
 [int]$num = 0
-Get-Process | Select-Object name | Foreach { 
+invoke-command -computername $computer {Get-Process | Select-Object name} | Foreach { 
 	
-	[string]$procrun = "$_"       #adds each process appending to a string with comma as delimiter and new line at end of stirng
+	
+	
+	[string]$procrun = "$_" 
+	#adds each process appending to a string with comma as delimiter and new line at end of stirng
 	[string]$procrun1 = $procrun.trim("@{Name=")
-	[string]$process = $procrun1.trimend("}")
-	[string]$proclist = $proclist + $process + ","
+	[string]$procposition = $procrun1.IndexOf(";") #finds position of semicolon in list of processes
+	[string]$process = $procrun1.substring(0, $procposition)
+	[string]$proclist = $proclist + $process + ","	
 	}
 	
 	writeLog ("Processes|$proclist")
 	
-Get-WmiObject -Class win32_Product | Select-Object name | Foreach {   #adds each application appending it to a string with a comma as a delimiter and new line at end of string
+
+	invoke-command -computername $computer {Get-WmiObject -Class win32_Product | Select-Object name} | Foreach {   #adds each application appending it to a string with a comma as a delimiter and new line at end of string
 	[string]$apprun = "$_"
-	[string]$apprun2 = $apprun.trimstart("@{name=")
-	[string]$apprun3 = $apprun2.trimend("}")
-	[string]$applist = $applist + $apprun3 + ","
+	[string]$apprun1 = $apprun.trimstart("@{name=")
+	[string]$applposition = $apprun1.IndexOf(";")
+	[string]$apprun2 = $apprun1.substring(0, $applposition)
+	[string]$applist = $applist + $apprun2 + ","
+	
 	}
 	writeLog("Applications|$applist")
 	
 	
-writeLog("End of logfile.")
+Rename-Item "C:\Users\Administrator\Desktop\GitHub\SysAgNT-464_2131-stanley_discavage\sysAgntRemote.log" "C:\Users\Administrator\Desktop\GitHub\SysAgNT-464_2131-stanley_discavage\$datename.log"
 
+Move-Item "C:\Users\Administrator\Desktop\GitHub\SysAgNT-464_2131-stanley_discavage\$datename.log"  "C:\Users\SysAgNT\site\Dropbox\site\data\$computer\$datename.log"
+}
